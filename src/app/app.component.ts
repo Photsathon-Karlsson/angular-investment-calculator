0ddf93ae1ed8f91e76349d48df80b194fbdcc229
KoiOriginal <photsathon.sangsukwaw@gmail.com>
2026-09-13 11:54:45 +0200

    Add header with investment calculator logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
+// Controls the main application component
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+// Controls the header component
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {}
